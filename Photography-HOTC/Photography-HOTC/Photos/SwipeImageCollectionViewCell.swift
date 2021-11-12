fab4d878fc81a4acc0d547740290bb3c3daa7a4b
amruthas0494 <90743732+amruthas0494@users.noreply.github.com>
2021-11-12 09:45:13 +0530

    Add files via upload

---
+++ Photography-HOTC/Photography-HOTC/Photos/SwipeImageCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SwipeImageCollectionViewCell.swift
+//  Photography-HOTC
+//
+//  Created by apple on 02/11/21.
+//  Copyright © 2021 apple. All rights reserved.
+//
+
+import UIKit
+
+class SwipeImageCollectionViewCell: UICollectionViewCell {
+   
+    
+    @IBOutlet weak var swipeImage: UIImageView!
+}
