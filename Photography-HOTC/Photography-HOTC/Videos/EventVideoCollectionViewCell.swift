534097753370795b7ed816a84567e0db569b9a9f
amruthas0494 <90743732+amruthas0494@users.noreply.github.com>
2021-11-26 00:11:04 +0530

    Add files via upload

---
+++ Photography-HOTC/Photography-HOTC/Videos/EventVideoCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  EventVideoCollectionViewCell.swift
+//  HOTCC
+//
+//  Created by apple on 27/10/21.
+//  Copyright © 2021 apple. All rights reserved.
+//
+
+import UIKit
+
+
+class EventVideoCollectionViewCell: UICollectionViewCell {
+    
+   
+
+    
+    @IBOutlet weak var displayTumbnail: UIImageView!
+    
+    @IBOutlet weak var videoButton: UIButton!
+   
+}
