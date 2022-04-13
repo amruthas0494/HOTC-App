b7dc026eae92f9500b3f31d01c428e0570a6ad82
amruthas04 <amrutha.s@cognitiveclouds.com>
2022-04-13 13:10:10 +0530

    fixed up swipe and folder hiding changes

---
+++ Photography-HOTC/Pods/Target Support Files/Pods-Photography-HOTC-Photography-HOTCUITests/Pods-Photography-HOTC-Photography-HOTCUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Photography_HOTC_Photography_HOTCUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Photography_HOTC_Photography_HOTCUITests
+@end
---
+++ Photography-HOTC/Pods/Target Support Files/Pods-Photography-HOTCTests/Pods-Photography-HOTCTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Photography_HOTCTests : NSObject
+@end
+@implementation PodsDummy_Pods_Photography_HOTCTests
+@end
